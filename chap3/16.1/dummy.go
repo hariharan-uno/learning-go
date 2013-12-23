0a5e01f547bce131a6b6df9425bbabab77426e4d
Hari haran <hariharan.uno@gmail.com>
2013-12-23 11:56:12 +0530

    Modified chap3-16.1

--- chap3/16.1/dummy.go
+++ chap3/16.1/dummy.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/hariharan-uno/learning-go/chap3/stack"
+	"github.com/hariharan-uno/learning-go/chap3/16.1/stack"
 )
 
 func main() {
 	s := new(stack.Stack) //Remember this! Usage of exported types
 	s.Push(25)
-	fmt.Printf("stack %v\n", s)
+	fmt.Printf("%d\n", s.Data[0])
 	s.Push(14)
 	fmt.Printf("stack %v\n", s)
 	fmt.Printf("%d\n", s.Pop())
--- chap3/16.1/stack/stackpack.go
+++ chap3/16.1/stack/stackpack.go
@@ -3,24 +3,24 @@ and simple push and pop functions for stack manipulations*/
 package stack
 
 type Stack struct {
-	i    int
-	data [10]int
+	I    int
+	Data [10]int
 }
 
 /*We use a pointer here so that the function works
 on the actual variable and not a copy of it*/
 
-//Push pushes an int on the stack 
+//Push pushes an int on the stack
 func (s *Stack) Push(k int) {
-	if s.i+1 > 9 {
+	if s.I+1 > 9 {
 		return
 	}
-	s.data[s.i] = k
-	s.i++
+	s.Data[s.I] = k
+	s.I++
 }
 
 //Pop pops out an item from the stack
 func (s *Stack) Pop() int {
-	s.i--
-	return s.data[s.i]
+	s.I--
+	return s.Data[s.I]
 }
